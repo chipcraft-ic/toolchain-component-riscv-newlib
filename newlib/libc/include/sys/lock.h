ca44a2a3ca029ef9312adabce2f5669d8a71a30d
Mateusz Jemielity <M.Jemielity@chipcraft-ic.com>
2021-08-09 11:42:28 +0200

    [CHIPCRAFT][NEWLIB][RISC-V] Import ChipCraft-specific patches to locking and multithreading.

--- newlib/libc/include/sys/lock.h
+++ newlib/libc/include/sys/lock.h
@@ -1,69 +1,60 @@
 #ifndef __SYS_LOCK_H__
 #define __SYS_LOCK_H__
 
-/* dummy lock routines for single-threaded aps */
+/*
+ * Lock routines for ccrv32 multicore apps. The implementation is provided by user.
+ */
 
-#include <newlib.h>
-#include <_ansi.h>
+typedef struct {
+    int core;
+} __lock_mutex_t;
 
-#if !defined(_RETARGETABLE_LOCKING)
+typedef struct {
+    int core;
+    int count;
+} __lock_recursive_mutex_t;
 
-typedef int _LOCK_T;
-typedef int _LOCK_RECURSIVE_T;
+typedef __lock_mutex_t _LOCK_T;
+typedef __lock_recursive_mutex_t _LOCK_RECURSIVE_T;
 
-#define __LOCK_INIT(class,lock) static int lock = 0;
-#define __LOCK_INIT_RECURSIVE(class,lock) static int lock = 0;
-#define __lock_init(lock) ((void) 0)
-#define __lock_init_recursive(lock) ((void) 0)
-#define __lock_close(lock) ((void) 0)
-#define __lock_close_recursive(lock) ((void) 0)
-#define __lock_acquire(lock) ((void) 0)
-#define __lock_acquire_recursive(lock) ((void) 0)
-#define __lock_try_acquire(lock) ((void) 0)
-#define __lock_try_acquire_recursive(lock) ((void) 0)
-#define __lock_release(lock) ((void) 0)
-#define __lock_release_recursive(lock) ((void) 0)
+#define __LOCK_INIT(class,lock) \
+    class _LOCK_T lock __attribute__ ((section (".locks"))) = { 0 }
 
-#else
+#define __LOCK_INIT_RECURSIVE(class,lock) \
+    class _LOCK_RECURSIVE_T lock __attribute__ ((section (".locks"))) = { 0 }
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+extern void __libc_lock_init(_LOCK_T *lock);
+extern void __libc_lock_init_recursive(_LOCK_RECURSIVE_T *lock);
+extern void __libc_lock_close(_LOCK_T *lock);
+extern void __libc_lock_close_recursive(_LOCK_RECURSIVE_T *lock);
+extern void __libc_lock_acquire(_LOCK_T *lock);
+extern void __libc_lock_acquire_recursive(_LOCK_RECURSIVE_T *lock);
+extern void __libc_lock_release(_LOCK_T *lock);
+extern void __libc_lock_release_recursive(_LOCK_RECURSIVE_T *lock);
 
-struct __lock;
-typedef struct __lock * _LOCK_T;
-#define _LOCK_RECURSIVE_T _LOCK_T
+/* Returns 0 for success and non-zero for failure */
+extern int __libc_lock_try_acquire(_LOCK_T *lock);
+extern int __libc_lock_try_acquire_recursive(_LOCK_RECURSIVE_T *lock);
 
-#define __LOCK_INIT(class,lock) extern struct __lock __lock_ ## lock; \
-	class _LOCK_T lock = &__lock_ ## lock
-#define __LOCK_INIT_RECURSIVE(class,lock) __LOCK_INIT(class,lock)
-
-extern void __retarget_lock_init(_LOCK_T *lock);
-#define __lock_init(lock) __retarget_lock_init(&lock)
-extern void __retarget_lock_init_recursive(_LOCK_T *lock);
-#define __lock_init_recursive(lock) __retarget_lock_init_recursive(&lock)
-extern void __retarget_lock_close(_LOCK_T lock);
-#define __lock_close(lock) __retarget_lock_close(lock)
-extern void __retarget_lock_close_recursive(_LOCK_T lock);
-#define __lock_close_recursive(lock) __retarget_lock_close_recursive(lock)
-extern void __retarget_lock_acquire(_LOCK_T lock);
-#define __lock_acquire(lock) __retarget_lock_acquire(lock)
-extern void __retarget_lock_acquire_recursive(_LOCK_T lock);
-#define __lock_acquire_recursive(lock) __retarget_lock_acquire_recursive(lock)
-extern int __retarget_lock_try_acquire(_LOCK_T lock);
-#define __lock_try_acquire(lock) __retarget_lock_try_acquire(lock)
-extern int __retarget_lock_try_acquire_recursive(_LOCK_T lock);
+#define __lock_init(lock) \
+    __libc_lock_init(&(lock))
+#define __lock_init_recursive(lock) \
+    __libc_lock_init_recursive(&(lock))
+#define __lock_close(lock) \
+    __libc_lock_close(&(lock))
+#define __lock_close_recursive(lock) \
+    __libc_lock_close_recursive(&(lock))
+#define __lock_acquire(lock) \
+    __libc_lock_acquire(&(lock))
+#define __lock_acquire_recursive(lock) \
+    __libc_lock_acquire_recursive(&(lock))
+#define __lock_try_acquire(lock) \
+    __libc_lock_try_acquire(&(lock))
 #define __lock_try_acquire_recursive(lock) \
-  __retarget_lock_try_acquire_recursive(lock)
-extern void __retarget_lock_release(_LOCK_T lock);
-#define __lock_release(lock) __retarget_lock_release(lock)
-extern void __retarget_lock_release_recursive(_LOCK_T lock);
-#define __lock_release_recursive(lock) __retarget_lock_release_recursive(lock)
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* !defined(_RETARGETABLE_LOCKING) */
+    __libc_lock_try_acquire_recursive(&(lock))
+#define __lock_release(lock) \
+    __libc_lock_release(&(lock))
+#define __lock_release_recursive(lock) \
+    __libc_lock_release_recursive(&(lock))
 
 #endif /* __SYS_LOCK_H__ */
